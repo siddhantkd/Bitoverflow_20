3d314c1d566fff9fba1ec40741a22984849b1ca5
siddhantkd <61938334+siddhantkd@users.noreply.github.com>
2020-09-12 00:54:54 +0530

    Added Home Layout

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var typed = new Typed('.type', {
+                    strings: [
+                      '" TO FREE AND OPEN SOURCE. "',
+                    ],
+
+                    typeSpeed: 70,
+                    backSpeed: 100,
+                    backDelay: 700,
+                    fadeOut: false,
+                    fadeOutClass: 'typed-fade-out',
+                    fadeOutDelay: 500,
+                    showCursor: true,
+                    cursorChar: '|',
+                    autoInsertCss: true
+                    // Default value
+
+                  });
